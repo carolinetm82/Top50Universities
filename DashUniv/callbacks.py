8f57979b91929179828b31ef7fe2155056fba022
carolinetm82 <thuymicao@gmail.com>
2020-12-06 23:04:49 +0100

    callbacks file updated

--- DashUniv/callbacks.py
+++ DashUniv/callbacks.py
@@ -1,6 +1,7 @@
 from dash.dependencies import Input, Output
-from layouts import df,urllib
+from layouts import df,df2,urllib
 from app import app
+from functions import px
 
 @app.callback(
     Output('download-link', 'href'),
@@ -10,10 +11,18 @@ def update_download_link(filter_value):
     csv_string = "data:text/csv;charset=utf-8," + urllib.parse.quote(csv_string)
     return csv_string
 
-'''
+
 @app.callback(
-    Output('app-2-display-value', 'children'),
-    Input('app-2-dropdown', 'value'))
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-'''
+    Output('research-vs-world_rank', 'figure'),
+    Input('dropdown-univ', 'value'))
+def update_figure(selected_country):
+    filtered_df = df2[df2.country == selected_country]
+
+# Create the scatter plot
+    fig1 = px.scatter(filtered_df, x="world_rank", y="research",
+                 hover_name="university_name",
+                 title="Research vs Rank for the top 50 universities (select a country with the scroll bar above)")
+
+    fig1.update_layout(transition_duration=500)
+
+    return fig1
